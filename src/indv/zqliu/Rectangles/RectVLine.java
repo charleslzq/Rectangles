c50bf083048137d276be578b609ffb838331ab75
zqliu <charleslzq@gmail.com>
2015-12-31 17:51:37 +0800

    add class graph

---
+++ src/indv/zqliu/Rectangles/RectVLine.java
@@ -0,0 +1,10 @@
+package indv.zqliu.Rectangles;
+
+public final class RectVLine extends Rectangle {
+
+	RectVLine(double a, double b, double c, double d) {
+		super(a, b, c, d);
+		// TODO Auto-generated constructor stub
+	}
+
+}
